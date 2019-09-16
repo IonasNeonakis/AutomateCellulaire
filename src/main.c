8f3759d84e412f1a62847c720d7ae82670b0ae85
Samir Toularhmine <samir.toularhmine@gmail.com>
2019-09-16 22:05:51 +0200

    Ajout de la fonction de conversion de decimal vers binaire

--- src/main.c
+++ src/main.c
@@ -2,12 +2,16 @@
 #include <stdlib.h>
 
 #include "../include/automate.h"
+#include "../include/utils.h"
 
 int main(int argc, char* argv[]){
 
     automate a = creer_automate(32, 16, 2);
     
+    char* nombre_binaire = conversion_decimal_binaire(256);
+
+    free(nombre_binaire);
     supprimer_automate(a);
-    
+
     return EXIT_SUCCESS;
 }
