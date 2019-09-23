6cbb2aed6f288f17a81c670512b8d9d681b19671
Samir Toularhmine <samir.toularhmine@gmail.com>
2019-09-23 23:47:47 +0200

    Fix bugs

--- src/main.c
+++ src/main.c
@@ -11,28 +11,26 @@ int main(int argc, char* argv[]){
     unsigned int dimension_max = 32;
     unsigned int nb_iterations = 16;
     //char config_initiale[241] = "000011110001001110000001100000000001111100000000000000000000111100000011100000000001100000000110000000000000000000010000000000111100000000001111111110000000001111000000000000000000001110000000000000000000000010000011000000000000000000000000\0";
-    char config_initiale[32]="0000000000000000000000000000000\0";
-    automate a = creer_automate(dimension_max, nb_iterations, 2);
+    char config_initiale[33]="00000000000000001000000000000000\0";
+    automate a = creer_automate(dimension_max, nb_iterations, 3);
 
     //somme = 0013100132 
     
     //binaire = 00011110
 
     //cel** tableau = generer_automate(a, "0123000001", &regle_somme, config_initiale, &afficher_cellule_somme);
-    set_regle(a, "00011110");
     
-    set_type_regle(a,&regle_binaire);
+    set_regle(a, "0013100132");
 
-    set_configuration_initiale(a, "00000000000000001000000000000000");
-    
+    set_type_regle(a,&regle_somme);
 
-    set_affichage_regle(a,&afficher_cellule_binaire);
+    set_configuration_initiale(a, config_initiale);
     
-    set_affichage(a,&afficher_automate_console);
-    //printf("je suis là\n");
+    set_affichage_regle(a, &afficher_cellule_somme);
+    
+    set_affichage(a, &afficher_automate_console);
 
     cel** tableau = generer_automate(a);
-    
 
     //affichage_automate_pgm(tableau, dimension_max, nb_iterations);
     //automate a;
