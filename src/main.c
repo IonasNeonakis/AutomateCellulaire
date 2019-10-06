ccd06f5c70babc5bc56f9588b12f1fd3ce44b253
Samir Toularhmine <samir.toularhmine@etu.univ-orleans.fr>
2019-10-06 17:06:17 +0200

    Ajouts de phrases d'aides

--- src/main.c
+++ src/main.c
@@ -58,7 +58,7 @@ int main(int argc, char* argv[]){
 
             process_args(a, argc, argv); // On effectue une lecture par argument de programme
 
-        }else if(argc == 2 && (!strcmp(argv[1], "-h" || !strcmp(argv[1], "--help")))){
+        }else if(argc == 2 && (!strcmp(argv[1], "-h") || !strcmp(argv[1], "--help"))){
 
             manuel(); // On affiche le manuel
             supprimer_regle(&r);
